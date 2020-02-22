47a2d528ddf5352f60836c6e1746d035c5d64c4d
gnought <1684105+gnought@users.noreply.github.com>
2020-02-22 22:45:10 +0800

    Pipe conn to mqtt stream parser and handle func

--- aedes.d.ts
+++ aedes.d.ts
@@ -68,7 +68,6 @@ declare namespace aedes {
     authorizeSubscribe?: AuthorizeSubscribeHandler
     authorizeForward?: AuthorizeForwardHandler
     published?: PublishedHandler
-    queueLimit?: number
     maxClientsIdLength?: number
   }
   interface Client extends EventEmitter {
